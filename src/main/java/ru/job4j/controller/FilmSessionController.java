8804800b458265491203a333f4687c15ecc9e2a7
XLoganxmenX <loganxmen97@gmail.com>
2024-07-27 10:51:36 +0900

    change view and controller for new FilmSessionPageDto

--- src/main/java/ru/job4j/controller/FilmSessionController.java
+++ src/main/java/ru/job4j/controller/FilmSessionController.java
@@ -5,38 +5,31 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.job4j.service.FileService;
-import ru.job4j.service.FilmService;
 import ru.job4j.service.FilmSessionService;
 
 @Controller
 @RequestMapping("/filmSessions")
 public class FilmSessionController {
     private final FilmSessionService filmSessionService;
-    private final FilmService filmService;
 
-    public FilmSessionController(FilmSessionService filmSessionService, FilmService filmService,
-                                 FileService fileService) {
+    public FilmSessionController(FilmSessionService filmSessionService) {
         this.filmSessionService = filmSessionService;
-        this.filmService = filmService;
     }
 
     @GetMapping
     public String getAll(Model model) {
-        model.addAttribute("filmSessionsDto", filmSessionService.findAll());
+        model.addAttribute("filmSessionsDto", filmSessionService.findAllFilmSessionDto());
         return "filmSessions/list";
     }
 
     @GetMapping("/{id}")
     public String getById(Model model, @PathVariable int id) {
-        var filmSessionOptional = filmSessionService.findById(id);
+        var filmSessionOptional = filmSessionService.getFilmSessionPageById(id);
         if (filmSessionOptional.isEmpty()) {
             model.addAttribute("message", "Сеанс или фильм с указанным идентификатором не найден");
             return "errors/404";
         }
-        model.addAttribute("filmSessionDto", filmSessionOptional.get());
-        var filmDto = filmService.findById(filmSessionOptional.get().getFilmId()).get();
-        model.addAttribute("filmDto", filmDto);
+        model.addAttribute("filmSessionPageDto", filmSessionOptional.get());
         return "filmSessions/one";
     }
 }
